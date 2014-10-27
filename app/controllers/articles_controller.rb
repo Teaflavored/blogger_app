7ae6917a21b1fce9f0ace59998c7348126f63e27
Auster Chen <Auster.s.chen@gmail.com>
2014-10-26 18:19:20 -0700

    Added show/create/destroy for articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,28 @@ class ArticlesController < ApplicationController
 	def show
 		@article = Article.find(params[:id])
 	end
+
+	def new
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		@article.save!
+
+		redirect_to @article
+	end
+
+	def destroy
+		Article.find(params[:id]).destroy!
+
+		redirect_to articles_path
+	end
+
+
+	private
+
+	def article_params
+		self.params.require(:article).permit(:title, :body)
+	end
 end
